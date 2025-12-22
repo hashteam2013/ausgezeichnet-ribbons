b28a9751b85db665b3714f9df7be1f1d86b5e990
hashsourabh <hashsourabh@gmail.com>
2025-12-22 13:35:12 +0530

    worked on responsive shop page

--- view/shop/index.php
+++ view/shop/index.php
@@ -1,28 +1,28 @@
 <div class="col-md-4 mob-blancer hidden">
     <a class="btn-cart" href="<?php echo make_url('cart');?>" title="<?php _e('View Cart'); ?>"><?php _e("Cart");?> <i class="fa fa-shopping-cart" aria-hidden="true"></i></a>&nbsp;&nbsp;
 </div>
-<div class="shop-search py-7 bg-body flex w-full">
+<div class="shop-search md:py-7 py-4 bg-body flex w-full">
     <div class="container-custom">
-        <div class="srch-bar flex items-center justify-center max-w-[820px] w-full mx-auto gap-2.5">
-            <div class="relative flex items-center flex-auto">
-                <svg width="24" height="24" class="absolute left-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div class="srch-bar flex items-center justify-center max-w-[820px] xs:flex-row flex-col w-full mx-auto gap-2.5">
+            <div class="relative flex items-center flex-auto xs:w-auto w-full">
+                <svg width="24" height="24" class="absolute left-5 md:w-6 md:h-6 w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#2D4D7E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                <input type="text" id="search-box" class="my-srch w-full rounded-xl bg-white focus:outline-none border-[#b1b1b1] border min-h-14 ps-[52px] text-base text-secondary font-medium" placeholder="Search">
+                <input type="text" id="search-box" class="my-srch w-full rounded-xl bg-white focus:outline-none border-[#b1b1b1] border md:min-h-14 min-h-10 md:ps-[52px] ps-10 md:text-base text-sm text-secondary font-medium" placeholder="Search">
             </div>
-           <input type="button" value="<?php _e('search') ?>" class="btn flex-1 max-w-[200px] min-h-14 text-lg font-medium cursor-pointer rounded-lg srch-btn search_ribbon bg-primary text-white">
+           <input type="button" value="<?php _e('search') ?>" class="btn flex-1 xs:w-auto w-full xs:max-w-[200px] md:min-h-14 min-h-10 md:text-lg text-sm font-medium cursor-pointer rounded-lg srch-btn search_ribbon bg-primary text-white">
         </div>
     </div>
 </div>
-<div class="flex xl:py-16 md:py-12 py-10 w-full">
+<div class="flex xl:py-16 md:py-10 py-8 w-full">
     <div class="container-custom">
-        <div class="flex lg:flex-row flex-col 2xl:gap-14 xl:gap-10 gap-5">
+        <div class="flex lg:flex-row flex-col 2xl:gap-14 xl:gap-6 gap-5">
             <!-----------------------Side-bar------------------------->
             <div class="high flex flex-col gap-5 lg:w-[22%] w-full">
                 <h3 class="xl:text-3xl lg:text-2xl text-xl text-black font-gothic">Filters</h3>
-                <div class="flex flex-col bg-body rounded-xl p-5 gap-5">
+                <div class="flex flex-col bg-body rounded-xl xl:p-5 lg:p-3 p-5 gap-5">
                     <div class="categories">
-                        <div class="cat-heading text-[22px] leading-[32px] text-black font-semibold"><?php _e("Categories"); ?></div>
+                        <div class="cat-heading xl:text-[22px] xl:leading-[32px] lg:text-xl text-lg text-black font-semibold"><?php _e("Categories"); ?></div>
                         <ul class="mt-5 flex flex-col gap-4 w-full">
                             <?php foreach ($categories as $key=>$cat) { ?>
                                 <li class="text-dark shop-cat-list font-medium text-sm"><label class="relative flex items-center gap-2"><input type="checkbox" name="categories_name[]" class="cat_class min-w-[26px] h-[26px] absolute left-0 z-[1] opacity-0" id="cat_id_<?php echo $cat->position ?>"  dist-attr="<?php echo $cat->is_district_related; ?>" showclosed="<?php echo $cat->show_closed; ?>" value="<?php echo $cat->id ?>"> <span class="checkmark relative min-w-[26px] h-[26px] border-[1.5px] border-secondary inline-flex rounded-[5px]"></span><?php echo $cat->{'name_'.$app['language']}; ?></label></li>
@@ -30,7 +30,7 @@
                         </ul>
                     </div>
                     <div class="categories dist">
-                        <div class="cat-heading text-[22px] leading-[32px] text-black font-semibold"><?php _e("Districts"); ?></div>
+                        <div class="cat-heading xl:text-[22px] xl:leading-[32px] lg:text-xl text-lg text-black font-semibold"><?php _e("Districts"); ?></div>
                         <ul class="dist_ul mt-5 flex flex-col gap-4 w-full">
                             <?php foreach ($districts as $dist) { ?>
                                 <li class="text-dark shop-cat-list  font-medium text-sm">
@@ -40,7 +40,7 @@
                         </ul>
                     </div>
                     <div class="categories depart"> 
-                        <div class="cat-heading text-[22px] leading-[32px] text-black font-semibold"><?php _e("Collections"); ?></div> 
+                        <div class="cat-heading xl:text-[22px] xl:leading-[32px] lg:text-xl text-lg text-black font-semibold"><?php _e("Collections"); ?></div> 
                         <ul class="mt-5 flex flex-col gap-4 w-full"> 
                             <?php foreach ($additional_categories as $key=>$add_cat) { ?> 
                                 <li class="text-dark shop-cat-list  font-medium text-sm">
@@ -53,7 +53,7 @@
             </div> 
             <!-----------------------Side-bar-end------------------------->
             <div class="srch-reslt flex flex-col gap-5 lg:w-[47%] w-full">
-                <h3 class="xl:text-3xl text-2xl text-black font-gothic">Product Categories</h3>
+                <h3 class="xl:text-3xl lg:text-2xl  text-xl text-black font-gothic">Product Categories</h3>
                 <!-- <div class="srch-heading"><?php _e("Search Result by category!"); ?></div> -->
                 <div class="bdr">
                     <div class="flag-sec">
@@ -76,17 +76,17 @@
                     </div>
                 </div>    
             </div>
-            <?php// echo "ddd"pr($app['logged_in_user']); ?>
+           
             <div class="lg:w-[31%] w-full flex flex-col gap-5">
                 <h3 class="xl:text-3xl lg:text-2xl text-xl text-black font-gothic">Customer Listing</h3>
                 <!-- <div class="srch-heading"><?php _e("Selected Items"); ?></div> -->
-                <div class="bg-body p-5 rounded-[20px] border border-[#D9D9D9]">
+                <div class="bg-body 2xl:p-5 p-3 rounded-[20px] border border-[#D9D9D9]">
                     <div class="srch-reslt slect">
                             <div class="padd-side view-shops">
-                                <div class="flex xl:flex-row lg:flex-col flex-row xl:items-center gap-5 mb-5">
+                                <div class="flex xl:flex-row lg:flex-col xs:flex-row flex-col xl:items-center 2xl:gap-5 gap-2 mb-5">
                                     <?php if (isset($app['logged_in_user']) && $app['logged_in_user'] != '') { //pr($customers);?>
                                     <?php if(!empty($customers)){ ?>
-                                        <select id="custm" class="min-h-[46px] focus:outline-none cursor-pointer flex-1 text-black text-base font-regular rounded-[10px] 2xl:px-5 px-2 max-w-[227px] w-full border border-[#D9D9D9]"><?php
+                                        <select id="custm" class="min-h-[46px] focus:outline-none cursor-pointer flex-1 text-black text-base font-regular rounded-[10px] 2xl:px-5 px-2 xl:max-w-[227px] lg:w-full w-auto border border-[#D9D9D9]"><?php
                                             foreach ($customers as $cust){ //pr($cust);
                                                 if(!empty($cust)){ ?>
                                                 <option value="<?php echo $cust['id']; ?>"><?php echo $cust['first_name'].' '.$cust['last_name'] ;  ?></option>
@@ -108,7 +108,7 @@
                                     </div>
                                 </div>
                                 <?php if (isset($app['logged_in_user']) && $app['logged_in_user']!="") { ?>
-                                <div class="mb-5 flex gap-2.5 items-center justify-between">
+                                <div class="mb-5 flex gap-2.5 items-center lg:justify-between">
                                     <h4 class="text-secondary xl:text-xl text-lg font-medium font-gothic">Filter</h4>
                                     <div id='show_buttons'>
                                         <?php 
@@ -122,7 +122,7 @@
                                         <?php } ?>
                                     </div>
                                 </div>
-                                <div class="check-tag flex flex-wrap bg-white rounded-[10px] p-5 border border-[#D9D9D9]">
+                                <div class="check-tag flex flex-wrap bg-white rounded-[10px] xl:p-5 p-3 border border-[#D9D9D9]">
                                     <div class="w-full">
                                         <ul class="batch w-full flex flex-col">
                                                  <?php 
@@ -152,7 +152,7 @@
                                                 
                                                 ?>
 
-                                            <li class="mb-5 pb-5 border-b border-[#D9D9D9] last:pb-0 last:mb-0 last:border-none flex gap-2.5 customer-list" id="ribbon_<?php echo $batch['id'] . $ordered_str ?>>
+                                            <li class="xl:mb-5 xl:pb-5 mb-4 pb-4 border-b border-[#D9D9D9] last:pb-0 last:mb-0 last:border-none flex gap-2.5 customer-list" id="ribbon_<?php echo $batch['id'] . $ordered_str ?>>
 
                                                 <label class='relative'><input type='checkbox' class='chkid min-w-4 h-4 absolute left-0 z-[1] opacity-0' name='o<?php echo $ordered_int ?>' value='<?php echo $batch['id']; ?>'><span class='smallcheck relative min-w-4 h-4 border border-[#B1B1B1] inline-flex rounded-[2px]'></span></label>
 
@@ -165,9 +165,9 @@
                                         </ul>
                                     </div>
                                 </div>
-                                <div class="flex xl:gap-5 gap-2.5 mt-10 w-full">
-                                        <a href = "javascript:void(0)" class="flex-1"><input type="button" class="cursor-pointer cart-slct w-full text-white font-medium rounded-lg  hover:bg-primary min-h-[46px] text-base bg-secondary divider  add_to_cart_ribbon" value="<?php _e("Add to Cart"); ?>"></a>
-                                        <a class="flex-1" href = "<?php echo make_url('cart');?>"><input type="button" class="cart-slct w-full text-white rounded-lg cursor-pointer hover:bg-primary min-h-[46px] text-base bg-secondary font-medium divider buy  view_cart" value="<?php _e("View Cart"); ?>"></a>
+                                <div class="flex 2xl:flex-row lg:flex-col xs:flex-row flex-col md:flex-nowrap flex-wrap xl:gap-3 gap-2.5 xl:mt-10 mt-5 w-full">
+                                        <a href = "javascript:void(0)" class="flex-1"><input type="button" class="cursor-pointer cart-slct w-full text-white font-medium rounded-lg  hover:bg-primary min-h-[46px] text-base bg-secondary divider md:px-0 px-2  add_to_cart_ribbon" value="<?php _e("Add to Cart"); ?>"></a>
+                                        <a class="flex-1" href = "<?php echo make_url('cart');?>"><input type="button" class="cart-slct w-full text-white rounded-lg cursor-pointer hover:bg-primary min-h-[46px] text-base bg-secondary font-medium divider md:px-0 px-2 buy  view_cart" value="<?php _e("View Cart"); ?>"></a>
                                 </div>
                                 <?php }?>
                            </div>
@@ -181,7 +181,7 @@
                   <div class="srch-heading">
                         <h3 class="xl:text-3xl lg:text-2xl text-xl text-black font-gothic capitalize"><?php _e("Badges Placed");?></h3>
                   </div>
-                  <div class='badges bg-body p-5 rounded-[20px] border border-[#D9D9D9]'>
+                  <div class='badges bg-body 2xl:p-5 p-3 rounded-[20px] border border-[#D9D9D9]'>
                         <?php //foreach($selected_customer_batches as $custID){}
                         if (isset($app['logged_in_user']) && $app['logged_in_user'] != '' && !empty($selected_customer_batches)) {  ?>
                         <div class="srch-reslt slect mar-top-10">
